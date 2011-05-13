02f899340c92291437723cd1ff58f3dc91c1621c
matthew.barulic@gmail.com <matthew.barulic@gmail.com@b3a443fd-0f55-9a23-ba91-2c4df1bf3164>
2011-05-13 22:34:25 +0000

    Added top credit comment.

--- JCVKinectDelegate.java
+++ JCVKinectDelegate.java
@@ -1,3 +1,11 @@
+/*
+ * This class is a part of JCVKinect a project developed by Nathan Daniel and Matt Barulic
+ * under the supervision of Mr. Reinald Yoder in Marist School's 2011
+ * Advanced Computer Science seminar.
+ * Feel free to use this code and your projects and provide any feedback or improvements.
+ * We only ask that you keep this and the above lines intact in your edits. Thank you.
+ */
+ 
 package com.marist.cs2011;
 import static com.googlecode.javacv.cpp.opencv_core.*;
 
